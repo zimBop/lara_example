e8c0caa9f1ffd32da8714ff832f276d7184a2cde
glebovgin <glebovgin@gmail.com>
2020-04-07 02:56:17 +0300

    [EL-7] Update js/css libs versions, code formatting

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,14 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Login</title>
-    <meta name="robots" content="noindex" />
+    <meta name="robots" content="noindex"/>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+          integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ mix('css/admin/login.min.css') }}" />
+    <link rel="stylesheet" href="{{ mix('css/admin/login.min.css') }}"/>
 
     <!-- ReCaptcha -->
     {{--<script src='https://www.google.com/recaptcha/api.js?render=6LcFWYEUAAAAALnp53TQ2x2oUwOHMYHt4Y9p_VUm'></script>--}}
@@ -24,12 +26,16 @@
             <div class="card card-block fadeIn animated">
                 <div class="p-3">
                     <div class="form-side">
-                        <form action="{{ route(R_ADMIN_LOGIN_SUBMIT) }}" method="post" class="form-horizontal mt-2" id="auth-form">
+                        <form action="{{ route(R_ADMIN_LOGIN_SUBMIT) }}" method="post" class="form-horizontal mt-2"
+                              id="auth-form">
                             @csrf
                             {{--<input type="hidden" name="g-recaptcha-response" class="g-token" value="">--}}
                             <div class="form-group row">
                                 <div class="col-12">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
+                                    <input id="email" type="email"
+                                           class="form-control @error('email') is-invalid @enderror" name="email"
+                                           value="{{ old('email') }}" required autocomplete="email" autofocus
+                                           placeholder="Email">
 
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -40,7 +46,9 @@
                             </div>
                             <div class="form-group row">
                                 <div class="col-12">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+                                    <input id="password" type="password"
+                                           class="form-control @error('password') is-invalid @enderror" name="password"
+                                           required autocomplete="current-password" placeholder="Password">
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -51,13 +59,16 @@
                             </div>
                             <div class="form-group text-center row">
                                 <div class="col-12">
-                                    <button class="btn btn-info btn-block" type="submit" name="submit" value="1">Sign in</button>
+                                    <button class="btn btn-info btn-block" type="submit" name="submit" value="1">Sign
+                                        in
+                                    </button>
                                 </div>
                             </div>
 
                             {{-- Message Alert --}}
                             @if(session('message'))
-                                <div class="alert alert-{{ session('alert_type') }} alert-dismissible fade show" role="alert">
+                                <div class="alert alert-{{ session('alert_type') }} alert-dismissible fade show"
+                                     role="alert">
                                     {{ session('message') }}
                                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
@@ -72,8 +83,12 @@
         </div>
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
 
 {{-- TODO: Captcha--}}
 
