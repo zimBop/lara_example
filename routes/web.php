a6b1c670be4b43a7c41bad4592b2a203429d462f
glebovgin <glebovgin@gmail.com>
2020-04-07 02:21:49 +0300

    [EL-7] Add admin routes

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], static function () {
+    Route::get('/login', 'LoginController@showLoginForm')->name(R_ADMIN_LOGIN);
+    Route::post('/login', 'LoginController@login')->middleware('throttle:5,1')->name(R_ADMIN_LOGIN_SUBMIT);
+    Route::get('/logout', 'LoginController@logout')->name(R_ADMIN_LOGOUT);
+    Route::get('/', 'DashboardController@index')->name(R_ADMIN_DASHBOARD);
+
+    Route::middleware('auth:admin')
+        ->group(static function () {
+
+            Route::prefix('clients')
+                ->group(static function(){
+                    Route::get('/', 'DashboardController@index')->name(R_ADMIN_CLIENTS_LIST);
+                });
+        });
+});
