748b8e6ec57824a2bf0a33a8d7f80f60bf5077d3
glebovgin <glebovgin@gmail.com>
2020-04-07 02:18:36 +0300

    [EL-7] Add npm packages and dependencies + webpack config for admin section

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,11 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    //.js('resources/js/app.js', 'public/js')
+    .js('resources/js/admin/custom.js', 'public/js/admin/custom.min.js')
+    //.sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/admin/custom.scss', 'public/css/admin/custom.min.css')
+    .sass('resources/sass/admin/login.scss', 'public/css/admin/login.min.css')
+    .copyDirectory('resources/images/admin', 'public/images/admin')
+    .version();
