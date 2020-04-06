5fcb1e4998b8291f3151dbdcc46fd9c8cf7acdb6
glebovgin <glebovgin@gmail.com>
2020-04-07 02:21:06 +0300

    [EL-7] Add names.php autoload into composer.json with Defined names for Routes
    Also add laravel/ui + dependencies for Auth scaffolding.

---
+++ routes/names.php
@@ -0,0 +1,10 @@
+<?php
+
+define('R_HOME', 'home');
+
+define('R_ADMIN_LOGIN', 'admin.login');
+define('R_ADMIN_LOGIN_SUBMIT', 'admin.login.submit');
+define('R_ADMIN_LOGOUT', 'admin.logout');
+define('R_ADMIN_DASHBOARD', 'admin.dashboard');
+
+define('R_ADMIN_CLIENTS_LIST', 'admin.clients.list');
