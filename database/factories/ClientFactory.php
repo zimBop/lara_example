d56313c2de8421b936c2ed5d25a6cd3658a058e8
glebovgin <glebovgin@gmail.com>
2020-04-09 00:37:25 +0300

    [EL-17] Update Client Factory: add is_active column

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -5,7 +5,7 @@
 use App\Models\Client;
 use Faker\Generator as Faker;
 
-$factory->define(Client::class, function (Faker $faker) {
+$factory->define(Client::class, static function (Faker $faker) {
     return [
         Client::FIRST_NAME => $faker->firstName,
         Client::LAST_NAME => $faker->lastName,
@@ -14,5 +14,6 @@ $factory->define(Client::class, function (Faker $faker) {
         Client::EMAIL_VERIFIED_AT => now(),
         Client::PASSWORD => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         Client::BIRTHDAY => $faker->date(),
+        Client::IS_ACTIVE => $faker->boolean()
     ];
 });
