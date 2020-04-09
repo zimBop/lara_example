7e38cd02219177eca3fa793686b3b21728c6fc21
ZimBop <miterkr@gmail.com>
2020-04-09 11:51:29 +0300

    [EL-13] Add ClientResetPasswordTest

---
+++ tests/Feature/Client/ClientResetPasswordTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature\Client;
+
+use App\Models\Client;
+use App\Services\ResetPasswordService;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Hash;
+
+class ClientResetPasswordTest extends TestCase
+{
+    public function testResetPassword()
+    {
+        $client = factory(Client::class)->create();
+        $passwordService = $this->app->make(ResetPasswordService::class);
+        $passwordService->setClient($client);
+        $token = $passwordService->create();
+
+        Passport::actingAs($client, ['access-client']);
+
+        $newPassword = $this->faker->password;
+
+        $data = [
+            Client::PASSWORD => $newPassword,
+            'token' => $token,
+        ];
+
+        $response = $this->patchJson(
+            route('clients.reset-password', ['client' => $client->id]),
+            $data
+        );
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                 'done' => true,
+                 'message' => 'Password reset successfully.',
+             ]);
+
+        $client->refresh();
+        $this->assertTrue(Hash::check($newPassword, $client->password));
+
+        $this->checkExpiredToken($client, $data);
+    }
+
+    protected function checkExpiredToken(Client $client, array $data)
+    {
+        $tokenModel = $client->passwordResetToken;
+        $tokenLifetime = config('app.password_reset.token_lifetime');
+        $tokenModel->created_at = $tokenModel->created_at->subMinutes($tokenLifetime);
+        $tokenModel->save();
+
+        $response = $this->patchJson(
+            route('clients.reset-password', ['client' => $client->id]),
+            $data
+        );
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                 'done' => false,
+                 'message' => 'Password reset token expired or not exists.',
+             ]);
+    }
+}
