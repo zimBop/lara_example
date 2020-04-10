6700db0742ad3ba9815602223b506fd76e6f07b4
glebovgin <glebovgin@gmail.com>
2020-04-10 20:24:49 +0300

    [EL-17] Update ClientFactory
    * Add data for created_at and updated_at columns
    * Change data source for email_verified_at column

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -11,9 +11,11 @@ $factory->define(Client::class, static function (Faker $faker) {
         Client::LAST_NAME => $faker->lastName,
         Client::PHONE => \App\Services\ClientService::generatePhoneNumber(),
         Client::EMAIL => $faker->unique()->safeEmail,
-        Client::EMAIL_VERIFIED_AT => now(),
+        Client::EMAIL_VERIFIED_AT => $faker->dateTimeBetween('-6 days', '-5 days', 'America/New_York'),
         Client::PASSWORD => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         Client::BIRTHDAY => $faker->date(),
-        Client::IS_ACTIVE => $faker->boolean()
+        Client::IS_ACTIVE => $faker->boolean(),
+        Client::CREATED_AT => $faker->dateTimeBetween('-11 days', '-7 days', 'America/New_York'),
+        Client::UPDATED_AT => $faker->dateTimeBetween('-4 days', 'now', 'America/New_York'),
     ];
 });
