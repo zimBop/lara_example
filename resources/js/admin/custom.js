0bc1416cbc6aaddf8457a02fa93e6599d6614013
glebovgin <glebovgin@gmail.com>
2020-04-10 20:32:42 +0300

    [EL-17] Add js-handling code for client activity switch + bundled assets

--- resources/js/admin/custom.js
+++ resources/js/admin/custom.js
@@ -8,4 +8,22 @@ $(function(){
 
     // Enable tooltips
     $('[data-toggle="tooltip"]').tooltip();
+
+    // Clients list
+    // Toggle activity switch
+    $('.client-activity-switch').on('change', function () {
+        let clientId = $(this).data('client-id'),
+            isChecked = $(this).attr('checked'),
+            that = $(this);
+
+        $.post(ROUTES.R_ADMIN_AJAX_CLIENTS_CHANGE_ACTIVITY, {clientId : clientId}, function(data){
+            if(data.status === 'success') {
+                $.jGrowl("Availability successfully changed");
+                $(that).attr('checked', !isChecked);
+                $(that).parent('div').attr('title', !isChecked ? 'Active' : 'Inactive');
+            } else {
+                alert('Sorry, an issue occurred. Please, contact developers.')
+            }
+        }, 'json');
+    });
 });
