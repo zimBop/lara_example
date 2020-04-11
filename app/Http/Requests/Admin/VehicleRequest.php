fad23defea93420ef9267ecddff8f5a7f86443a9
glebovgin <glebovgin@gmail.com>
2020-04-11 18:16:56 +0300

    [EL-18] Add VehicleRequest and ArrayKeyExists for data validation

---
+++ app/Http/Requests/Admin/VehicleRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Rules\Admin\ArrayKeyExists;
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Vehicle;
+use App\Constants\VehicleConstants;
+
+class VehicleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            Vehicle::LICENSE_PLATE => ['required', 'string', 'max:15'],
+            Vehicle::BRAND_ID => ['required', 'integer', new ArrayKeyExists(VehicleConstants::BRANDS)],
+            Vehicle::MODEL_ID => ['required', 'integer', new ArrayKeyExists(VehicleConstants::BRANDS[request()->brand_id]['models'])],
+            Vehicle::COLOR_ID => ['nullable', 'integer', new ArrayKeyExists(VehicleConstants::COLORS)],
+            Vehicle::STATUS_ID => ['required', 'integer', new ArrayKeyExists(VehicleConstants::STATUSES)],
+        ];
+    }
+}
