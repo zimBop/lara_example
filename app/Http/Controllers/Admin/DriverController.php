e774bdb116311fc5800701ec7f4bfd44a01ffed8
glebovgin <glebovgin@gmail.com>
2020-04-15 01:59:14 +0300

    [EL-33] Add create, edit, store, delete method in DriverController.php in Admin

--- app/Http/Controllers/Admin/DriverController.php
+++ app/Http/Controllers/Admin/DriverController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\DriverRequest;
 use Illuminate\Http\Request;
 use App\Models\Driver;
 
@@ -13,5 +14,34 @@ class DriverController extends Controller
     {
         $drivers = Driver::latest()->paginate(25);
         return view('admin.drivers.index', get_defined_vars());
+    }
+
+    public function create()
+    {
+        return view('admin.drivers.edit');
+    }
+
+    public function edit(Driver $driver)
+    {
+        return view('admin.drivers.edit', get_defined_vars());
+    }
+
+    public function store(DriverRequest $request, Driver $driver)
+    {
+        $data = $request->validated();
+        $driver->fill($data)->save();
+
+        return redirect()
+            ->route(R_ADMIN_DRIVERS_LIST)
+            ->with('success', 'Driver\'s information successfully saved');
+    }
+
+    public function delete(Driver $driver)
+    {
+        $driver->delete();
+
+        return redirect()
+            ->route(R_ADMIN_DRIVERS_LIST)
+            ->with('success', 'Driver successfully removed');
     }
 }
