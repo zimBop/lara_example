adbfa1dc92dec416de7a30b205ff3b4fbbe50295
glebovgin <glebovgin@gmail.com>
2020-04-15 02:09:24 +0300

    [EL-33] Add SoftDeletes trait in Driver model

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
@@ -35,7 +36,7 @@ use SMartins\PassportMultiauth\HasMultiAuthApiTokens;
  */
 class Driver extends Authenticatable
 {
-    use HasMultiAuthApiTokens, Notifiable;
+    use HasMultiAuthApiTokens, Notifiable, SoftDeletes;
 
     public const ID = 'id';
     public const FIRST_NAME = 'first_name';
