d17f7bf68d17a7754e2a639f9f78148dcd8d774b
ZimBop <miterkr@gmail.com>
2020-05-25 10:41:40 +0300

    [EL-68] Change messages for 'invite friend' feature

--- app/Constants/ClientMessages.php
+++ app/Constants/ClientMessages.php
@@ -6,6 +6,8 @@ class ClientMessages
 {
     public const PHONE_ALREADY_REGISTERED = 'The user with this phone is already registered with Electra.';
     public const ALL_INVITES_ALREADY_USED = 'Sorry, you’ve reached the limit of invites per person.';
+    // Message below will be used after weekly invites implemented
+    // public const ALL_INVITES_ALREADY_USED = 'Sorry, you have reached the limit of invites per person. Please check back next week.';
     public const INVITE_ALREADY_SENT = 'An invitation has already been sent to this phone number.';
     public const INVITE_TEXT = '%s invites you to ride green with Electra. \n'
         . 'Accept this invite below and your first trip is on us! \n'
